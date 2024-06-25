6d63114b6908338b53d03437ad721c663286d322
JonatanRek <gamerclassn7@gmail.com>
2024-06-25 20:13:00 +0200

    Additional Preparation done

---
+++ src/Livewire/Form.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SteelAnts\LivewireForm\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Str;
+
+class FormComponent extends Component
+{
+
+}
---
+++ src/LivewireFormServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SteelAnts\LivewireForm;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
+
+class LivewireFormServiceProvider extends ServiceProvider
+{
+    public function boot(Request $request)
+    {
+    }
+
+    public function register()
+    {
+    }
+}
